b93df0befea392cc160752509d0f943ad074d04a
JunYong <pjy1418@gmail.com>
2023-07-08 20:31:15 +0900

    feat: add product type

--- internal/product/domain/product_type.go
+++ internal/product/domain/product_type.go
@@ -1,1 +1,9 @@
 package domain
+
+type ProductType string // ProductType is a type of product
+
+const (
+	ProductTypeLoan   ProductType = "Loan"
+	ProductTypeFund   ProductType = "Fund"
+	ProductTypeEquity ProductType = "Equity"
+)
