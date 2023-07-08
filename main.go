2c088af681ae19e116a4dd09e7b8cda52d53b5b8
JunYong <pjy1418@gmail.com>
2023-07-08 21:17:57 +0900

    ci: testing build

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+	product "hillfolk.org/go-hexagonal-architecture/internal/product/domain"
+)
+
 func main() {
+
+	p := &product.Product{
+		ProductId:   "1",
+		ProductName: "Product 1",
+		ProductType: product.ProductTypeEquity,
+		Price: product.Currency{
+			Amount:   100,
+			Currency: "USD",
+		},
+	}
+
+	fmt.Println("Hello, Hexagonal Architecture!" + p.ToString())
+
 }
